67aae897352cf3a5b6a0cbc67bf20a3c0b65b495
Amanda Steinhebel <asteinhebel151@gmail.com>
2023-07-18 09:55:19 -0400

    add comment about outputs

--- run/run_interpret.sh
+++ run/run_interpret.sh
@@ -4,7 +4,9 @@
 #Assumes 	- directories for each target including data listed in file 'ft1.fits' 
 #Inputs 	- Target name, black hole mass (1e8 solar masses) and distance (Mpc) 
 #			_ Code pulls in computed SEDs from given targets
-#Outputs 	-
+#Outputs 	- .npy file of DM likelihood values for each combination of DM mass/crossSection in <TARGET>/output/plots/dloglike
+#			- likelihood plots in DM mass/crossSection space for every individual entry in <TARGET>/output/plots/
+#			- likelihood plot of all entries in <TARGETS> stacked together in top level directory
 
 # python schnittamn_likelihood_DM.py <target> <M_BH> <distance>
 
